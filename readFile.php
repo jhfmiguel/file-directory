00f5dc250e41007bb7efb7b46c2bd8dbc60a326f
jhfmiguel <jhfmiguel@hotmail.com>
2019-02-20 19:04:58 -0300

    third commit

--- readFile.php
+++ readFile.php
@@ -4,5 +4,15 @@ $filename = "csv/users.csv";
 if (file_exists($filename)) {
 	$file = fopen($filename, "r");
 	$headers = explode(",", fgets($file));
-	var_dump($headers);
+	$data = array();
+	while ($row = fgets($file)) {
+		$rowData = explode(",", $row);
+		$linha = array();
+		for ($i = 0; $i < count($headers); $i++) {
+			$linha[$headers[$i]] = $rowData[$i];
+		}
+		array_push($data, $linha);
+	}
+	fclose($file);
+	echo json_encode($data);
 }
